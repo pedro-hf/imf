c08e22212bb52e6dbb8baecb96ae8ff3e8832e43
Pedro Hernandez Fraczek <pedro.hf86@gmail.com>
2019-02-26 17:03:53 +0100

    Add new methods to the IMFclient class to extract other codes: countries, frequencies and indicators

--- imf_api/IMFClient.py
+++ imf_api/IMFClient.py
@@ -20,12 +20,44 @@ class IMFClient:
         data_json = d['CompactData']['DataSet']['Series']['Obs']
         return pd.DataFrame().from_dict(data_json)
 
-    def get_dataflow_codes(self, d='IFS'):
-        url = self.imf_url + '/DataStructure/' + d
+
+    def get_code_list_name(self, code_list, df):
+        if code_list == 'area':
+            return 'CL_AREA_{}'.format(df)
+        elif code_list == 'freq':
+            return 'CL_FREQ'
+        elif code_list == 'indicator':
+            return 'CL_INDICATOR_{}'.format(df)
+        else:
+            print('The possible code lists are area, freq or indicator')
+            return None
+
+
+    def get_dataflow_codes(self, df='IFS', code_list = 'indicator'):
+        """
+        Methods that returns the indicator codes for a specific indicator.
+        :param d: dataflow code
+        :return:
+        """
+        url = self.imf_url + '/DataStructure/' + df
         r = requests.get(url)
         d = r.json()
-        codes = pd.DataFrame().from_dict(d['Structure']['CodeLists']['CodeList'][3]['Code'])
+        ds_struct = pd.DataFrame().from_dict(d['Structure']['CodeLists']['CodeList'])
+        ds_struct.set_index('@id', inplace=True)
+
+        code_list_name = self.get_code_list_name(code_list, df)
+        codes = pd.DataFrame.from_dict(ds_struct.loc[code_list_name]['Code'])
         codes['Description'] = codes.Description.apply(lambda dct: dct['#text'])
         codes.rename(columns={'@value': 'code'}, inplace=True)
-        codes.set_index('code', inplace=True)
         return codes
+
+    def get_dataflows(self):
+        """Method that returns a pandas dataframe with the dataflows IDs and descriptions"""
+        url = self.imf_url + 'Dataflow'
+        r = requests.get(url)
+        data = r.json()
+        dataflows = pd.DataFrame.from_dict(data['Structure']['Dataflows']['Dataflow'])
+        dataflows['Description'] = dataflows.Name.apply(lambda d: d['#text'])
+        dataflows['dataflowID'] = dataflows.KeyFamilyRef.apply(lambda d: d['KeyFamilyID'])
+        dataflows = dataflows[['Description', 'dataflowID']]
+        return dataflows
