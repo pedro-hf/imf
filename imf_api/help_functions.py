2261c68273550f3277eb3fd28c15ade1918a3bd1
Pedro Hernandez Fraczek <pedro.hf86@gmail.com>
2019-02-28 12:52:16 +0100

    * Add changes to the dashboard, make scrollable list of countires in the dataflow (IFS) for now * Data now plots on the same plot, added some function in helpfunctions to deal with this (create_figure, create_go and create_layout)

--- imf_api/help_functions.py
+++ imf_api/help_functions.py
@@ -3,7 +3,6 @@ import pandas as pd
 from datetime import datetime
 
 
-
 def parse_imf_api_data(df):
     """
     It takes the respond from the IMF API and prepares it for javaScript plotly
@@ -33,3 +32,31 @@ def parse_imf_api_data(df):
 
     figures = dict(data=graph_one, layout=layout_one)
     return figures
+
+
+def create_go(name, df):
+    """Given a name (str) and a dataframe with columns time and value it will create a go.Scatter object"""
+    df['date'] = df.time.apply(lambda s: datetime.strptime(s, '%Y-%m'))
+    x_val = df.date.tolist()
+    y_val = df.value.tolist()
+    graph_object = go.Scatter(x=x_val,
+                              y=y_val,
+                              mode='lines',
+                              name=name)
+    return graph_object
+
+
+def create_layout(title='NA', x_label='NA', y_label='NA'):
+    """crates a layout dictionary for the figures, given axis labels and title"""
+    layout = {'title': title,
+              'xaxis': {'title': x_label,
+                        'autotick': False,
+                        'tick0': 1990,
+                        'dtick': 5},
+              'yaxis': {'title': y_label}}
+    return layout
+
+
+def create_figure(graph_objects, layout):
+    figure = dict(data=graph_objects, layout=layout)
+    return figure
--- imfapp/routes.py
+++ imfapp/routes.py
@@ -2,27 +2,33 @@ from imfapp import app
 from imf_api.IMFClient import IMFClient
 from flask import render_template
 from flask import request
-from imf_api.help_functions import parse_imf_api_data
+from imf_api.help_functions import parse_imf_api_data, create_go, create_figure, create_layout
 import json
 import plotly
+
 
 
 @app.route('/')
 @app.route('/dashboard.html', methods=['GET', 'POST'])
 def dashboard():
     if request.method == 'GET':
-        var = [['US', 'United States'], ['GE', 'Germany']]
+        client = IMFClient()
+        var = client.get_countries('IFS').values
         figures_json = json.dumps([], cls=plotly.utils.PlotlyJSONEncoder)
         return render_template('dashboard.html', figuresJSON=figures_json, var=var)
+
     elif request.method == 'POST':
         client = IMFClient()
         answer = request.form
         figures = []
-        print(answer)
+        graph_objects = []
         for selected_country in answer:
-            print(selected_country)
             data = client.get_data('IFS', 'M', selected_country, 'FILR_PA', '1950', '2016')
-            figures.append(parse_imf_api_data(data))
-        var = [['US', 'United States'], ['GE', 'Germany']]
+            graph_object = create_go(answer[selected_country], data)
+            graph_objects.append(graph_object)
+        layout = create_layout(title='FILR_PA', x_label='M', y_label='Something I don\'t know')
+        figure = create_figure(graph_objects, layout)
+        figures.append(figure)
+        var = client.get_countries('IFS').values
         figures_json = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
         return render_template('dashboard.html', figuresJSON=figures_json, var=var)
